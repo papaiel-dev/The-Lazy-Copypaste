c70a0bb183f974d1bd512aedef86149b1c2447d9
papaiel-dev <89oliveirarafael@gmail.com>
2025-09-20 22:07:22 -0300

    updates de segurança

--- middleware.js
+++ middleware.js
@@ -10,7 +10,6 @@ const PROTECTED_ROUTES = [
 export function middleware(request) {
   const token = request.cookies.get('sb-access-token');
 
-  // Protege as páginas internas
   if (PROTECTED_ROUTES.includes(request.nextUrl.pathname)) {
     if (!token) {
       const url = request.nextUrl.clone();
@@ -18,7 +17,7 @@ export function middleware(request) {
       return NextResponse.redirect(url);
     }
   }
-  
+
   return NextResponse.next();
 }
 
